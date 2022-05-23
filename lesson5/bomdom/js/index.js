5da04f81d3884d2cec6b09ebe911d27a4840c410
Diego Feresin <diegoferesin@gmail.com>
2022-05-22 21:40:09 -0300

    adding a home link

---
+++ lesson5/bomdom/js/index.js
@@ -0,0 +1,28 @@
+const inputElem = document.querySelector('input');
+const buttonElem = document.querySelector('button');
+const listElem = document.querySelector('ul');
+
+buttonElem.addEventListener('click', function () {
+    if (inputElem.value != '') {
+        let inputValue = inputElem.value;
+        inputElem.value = '';
+
+        let listItem = document.createElement('li');
+        let deleteButton = document.createElement('button');
+
+        deleteButton.setAttribute('aria-label', `Remove ${inputValue}`);
+
+        listItem.innerHTML = inputValue;
+        deleteButton.textContent = '❌';
+
+        listItem.appendChild(deleteButton);
+        listElem.appendChild(listItem);
+
+        deleteButton.addEventListener('click', function () {
+            listElem.removeChild(listItem);
+            input.focus();
+        });
+
+        input.focus();
+    }
+});
