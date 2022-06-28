f913289bf2f70e34726b94b00ebc6a091eabb3c9
Diego Feresin <diegoferesin@gmail.com>
2022-06-28 15:47:53 -0300

    Adding weather API

--- chamber/js/weather.js
+++ chamber/js/weather.js
@@ -1,6 +1,8 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
+const captionDesc = document.querySelector('#weather-sun');
+const windSpeed = document.querySelector('#wind-speed');
+const windChild = document.querySelector('#wind-child');
 
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=-34.599998&lon=-58.450001&units=metric&appid=1af0a868a3015b9525694dd97ced9a53&';
 
@@ -9,7 +11,7 @@ async function apiFetch() {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            // console.log(data); // this is for testing the call
+            console.log(data);
             displayResults(data);
         } else {
             throw Error(await response.text());
@@ -26,8 +28,24 @@ function displayResults(weatherData) {
 
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
+    const windSpeedData = weatherData.wind.speed;
 
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = desc;
+    captionDesc.innerHTML = desc;
+    windSpeed.innerHTML = windSpeedData;
+
+    let temperature = weatherData.main.temp;
+    if (temperature <= 10 && windSpeedData > 4.83) {
+        const windchill =
+            13.12 +
+            0.6215 * temperature -
+            11.37 * Math.pow(windSpeedData, 0.16) +
+            0.3965 * temperature * Math.pow(windSpeedData, 0.16);
+        windChild.innerHTML = `${Math.round(
+            windchill
+        )}°C`;
+    } else {
+        windChild.innerHTML = "N/A";
+    }
 }
