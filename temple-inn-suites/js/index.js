d0052fe457893c3ec8160ef4c999fc58d33a1bda
Diego Feresin <diegoferesin@gmail.com>
2022-07-18 12:12:09 -0300

    Adding script to get two random temples

--- temple-inn-suites/js/index.js
+++ temple-inn-suites/js/index.js
@@ -1,33 +1,27 @@
-let temples = document.querySelector('#temples');
-const urlTempleImages = "https://diegoferesin.github.io/wdd230/temple-inn-suites/data/temples-images.json";
-const urlTemples = "https://diegoferesin.github.io/wdd230/temple-inn-suites/data/temples.json";
-
-async function getTempleImages() {
-    try {
-        const response = await fetch(urlTempleImages);
-        if (response.ok) {
-            const images = await response.json();
-            console.log("Temples Images");
-            // images["images"].forEach(e => {
-            //     console.log(e);
-            // });
-        } else {
-            throw Error(await response.text());
-        }
-    } catch (error) {
-        console.log(error);
-    }
+function toggleMenu() {
+    document.getElementById("hamburger-nav").classList.toggle("open");
+    document.getElementById("hamburger-button").classList.toggle("open");
 }
+const hamburgerBtn = document.getElementById("hamburger-button");
+hamburgerBtn.onclick = toggleMenu;
 
-async function getTemples() {
+
+
+let temples = document.querySelector('#temples');
+const urlTemples = "https://diegoferesin.github.io/wdd230/temple-inn-suites/data/temples.json";
+const file = "./data/temples.json"
+
+async function getTemplesData() {
+    let templesData = [];
     try {
-        const response = await fetch(urlTemples);
+        const response = await fetch(file);
         if (response.ok) {
             const temples = await response.json();
-            console.log("Temples Name");
-            // temples["temples"].forEach(e => {
-            //     console.log(e);
-            // });
+            temples["temples"].forEach(e => {
+                templesData.push(e);
+            });
+            console.log(getRandomTempleInfo(templesData));
+            return getRandomTempleInfo(templesData);
         } else {
             throw Error(await response.text());
         }
@@ -36,6 +30,36 @@ async function getTemples() {
     }
 }
 
-getTempleImages();
-getTemples();
+function getRandomTempleInfo(templesData) {
+    templesData = templesData.filter(temple => {
+        if (temple.hasOwnProperty("src") && temple.hasOwnProperty("srcset")) {
+            if (temple.src.length > 0) {
+                return true
+            }
+        }
+    });
 
+    let firstTempleIndex = getRandomIndex(templesData);
+    let secondTempleIndex = getRandomIndex(templesData);
+    if (firstTempleIndex != secondTempleIndex) {
+        firstTempleData = templesData[firstTempleIndex]
+        secondTempleData = templesData[secondTempleIndex]
+        return [firstTempleData, secondTempleData]
+    } else {
+        let firstTempleIndex = getRandomIndex(templesData);
+        let secondTempleIndex = getRandomIndex(templesData);
+        firstTempleData = templesData[firstTempleIndex]
+        secondTempleData = templesData[secondTempleIndex]
+        return [firstTempleData, secondTempleData]
+    }
+}
+
+function getRandomIndex(templesArray) {
+    let min = 0
+    let max = templesArray.length
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+getTemplesData();
